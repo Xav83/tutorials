0ef20bf6efc3eefcd6a7d9628f392bf66ce7a84d
Xav83 <x.jouvenot@gmail.com>
2022-02-10 16:32:54 +0100

    Uses pip3 to install black solftware instead of pip since it is not available on the later one

--- Python format/run.sh
+++ Python format/run.sh
@@ -2,7 +2,7 @@
 
 echo "Installs black"
 export PATH=$PATH:/home/appveyor/.local/bin:/Users/appveyor/Library/Python/2.7/bin
-pip install black
+pip3 install black
 
 echo "Runs black to check if the python scripts are formatted correctly"
 black  --check .
