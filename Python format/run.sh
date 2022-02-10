684d539b218683ee220643163489169dd800896e
Xav83 <x.jouvenot@gmail.com>
2022-02-10 15:46:59 +0100

    Simplifies the CI processes by making black directly do the formatting check, instead of git

--- Python format/run.sh
+++ Python format/run.sh
@@ -4,8 +4,5 @@ echo "Installs black"
 export PATH=$PATH:/home/appveyor/.local/bin:/Users/appveyor/Library/Python/2.7/bin
 pip install black
 
-echo "Runs black to format the python scripts"
-black .
-
-echo "Checks if all the files versionned were already formatted"
-git diff --exit-code
+echo "Runs black to check if the python scripts are formatted correctly"
+black  --check .
